631db1eb8cf0f34b1d90ff7837fd4284c2187dd8
VinayParmar555 <vinayparmar111111@gmail.com>
2026-03-19 18:53:23 +0530

    fix pydantic deprecated defaults

--- app/schema/stats_schema.py
+++ app/schema/stats_schema.py
@@ -3,18 +3,14 @@ from typing import Dict, List
 
 
 class StatsResponse(BaseModel):
-    total_packets: int = Field(..., example=10000)
-    total_bytes: int = Field(..., example=5242880)
+    total_packets: int = Field(..., json_schema_extra={"example": 10000})
+    total_bytes: int = Field(..., json_schema_extra={"example": 5242880})
+    tcp_packets: int = Field(..., json_schema_extra={"example": 7000})
+    udp_packets: int = Field(..., json_schema_extra={"example": 3000})
+    forwarded_packets: int = Field(..., json_schema_extra={"example": 9500})
+    dropped_packets: int = Field(..., json_schema_extra={"example": 500})
 
-    tcp_packets: int = Field(..., example=7000)
-    udp_packets: int = Field(..., example=3000)
-
-    forwarded_packets: int = Field(..., example=9500)
-    dropped_packets: int = Field(..., example=500)
-
-    class Config:
-        from_attributes = True
-
+    model_config = {"from_attributes" : True}
 
 class AppStatsResponse(BaseModel):
     app_distribution: Dict[str, int] = Field(default_factory=dict)
