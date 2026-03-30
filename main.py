e08cfd2faea9acbb0729f6edc016f89450c28d03
VinayParmar555 <vinayparmar111111@gmail.com>
2026-03-30 12:09:41 +0530

    add pcap route

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from fastapi import FastAPI
 from app.schema.dpi_config_schema import DPIConfig
 from app.services.dpi_engine import DPIEngine
 from app.cache.redis import redis_manager
-from app.routes.pcap_routes import router as pcap_router
-from app.routes import ingest_routes, stats_routes, rules_routes
+from app.routes import pcap_routes, ingest_routes, stats_routes, rules_routes
 
 
 # -------------------------------------------------
@@ -42,7 +41,7 @@ app = FastAPI(
 # Register Routers
 # -------------------------------------------------
 
-app.include_router(pcap_router)
+app.include_router(pcap_routes.create_router(engine))
 app.include_router(ingest_routes.create_router(engine))
 app.include_router(stats_routes.create_router(engine))
 app.include_router(rules_routes.create_router(engine))
